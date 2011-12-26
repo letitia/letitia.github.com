446fdbe488c350fcc60dc1373791d1239b84ea8d
Letitia Lew <lew.letitia@gmail.com>
2011-12-26 20:55:43 +0800

    porting homepage from stanford.edu/~lletitia

---
+++ assets/scripts/header-nav.js
@@ -0,0 +1,7 @@
+
+document.write('<h1 id="name"><a href="index.html">Letitia Lew</a></h1>');
+document.write('<div id="nav_menu">');
+document.write('<ul id="main">');
+document.write('<li><a href="http://about.me/letitia.lew">About</a></li>');
+document.write('<li><a href="/projects/careercarabiner.html">Projects</a></li>');
+document.write('</ul></div>');
